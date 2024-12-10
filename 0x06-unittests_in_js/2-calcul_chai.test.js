adcb1ad7d4a892dbe9f83333dd68aa142fa19b67
VectorGits <davidoluremi0@gmail.com>
2024-12-10 16:56:51 +0100

    Refactor unit tests for calculateNumber function to use nested describe blocks for better organization

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,26 +2,32 @@ const { expect } = require('chai');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
-  it('should return the sum of rounded numbers', () => {
-    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
-    expect(calculateNumber('SUM', 1.2, 3.8)).to.equal(5);
-    expect(calculateNumber('SUM', 2.5, 2.5)).to.equal(6);
+  describe('SUM', () => {
+    it('should return the sum of rounded numbers', () => {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+      expect(calculateNumber('SUM', 1.2, 3.8)).to.equal(5);
+      expect(calculateNumber('SUM', 2.5, 2.5)).to.equal(6);
+    });
   });
 
-  it('should return the difference of rounded numbers', () => {
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-    expect(calculateNumber('SUBTRACT', 1.2, 3.8)).to.equal(-3);
-    expect(calculateNumber('SUBTRACT', 2.5, 2.5)).to.equal(0);
+  describe('SUBTRACT', () => {
+    it('should return the difference of rounded numbers', () => {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+      expect(calculateNumber('SUBTRACT', 1.2, 3.8)).to.equal(-3);
+      expect(calculateNumber('SUBTRACT', 2.5, 2.5)).to.equal(0);
+    });
   });
 
-  it('should return the division of rounded numbers', () => {
-    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
-    expect(calculateNumber('DIVIDE', 1.2, 3.8)).to.equal(0.25);
-    expect(calculateNumber('DIVIDE', 2.5, 2.5)).to.equal(1);
-  });
+  describe('DIVIDE', () => {
+    it('should return the division of rounded numbers', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+      expect(calculateNumber('DIVIDE', 1.2, 3.8)).to.equal(0.25);
+      expect(calculateNumber('DIVIDE', 2.5, 2.5)).to.equal(1);
+    });
 
-  it('should return "Error" when dividing by zero', () => {
-    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
-    expect(calculateNumber('DIVIDE', 1.2, 0.2)).to.equal('Error');
+    it('should return "Error" when dividing by zero', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+      expect(calculateNumber('DIVIDE', 1.2, 0.2)).to.equal('Error');
+    });
   });
 });
