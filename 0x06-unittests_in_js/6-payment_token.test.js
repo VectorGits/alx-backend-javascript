5826ffe981efa2cd6fca71244665fde9757798cf
VectorGits <davidoluremi0@gmail.com>
2024-12-11 19:16:41 +0100

    Update import statement in tests

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const PaymentToken = require('../6-payment_token');
+const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', function() {
   it('should return a resolved promise with the correct token', function(done) {
